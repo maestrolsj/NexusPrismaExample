c0a0e13ff90516e8cc4011a38ad52fe226d8b7db
SEJIN LEE <Sejin.Lee@softgarden.de>
2020-11-15 12:49:21 +0100

    example of nexus prisma

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,10 +1,16 @@
-import { ApolloServer } from "apollo-server-micro";
+import { ApolloServer } from "apollo-server";
 import { schema } from "src/schema";
+import { createContext } from "src/context";
 
-const server = new ApolloServer({ schema });
-const handler = server.createHandler({ path: "/api/grapthql" });
-
-export const config = {
-  api: { bodyParder: false },
-};
-export default handler;
+const server = new ApolloServer({
+  schema,
+  context: createContext,
+  tracing: process.env.NODE_ENV === "development",
+});
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}${server.graphqlPath}`);
+});
+// export const config = {
+//   api: { bodyParder: false },
+// };
+// export default handler;
--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,61 @@
-import { makeSchema, queryType } from "@nexus/schema";
+import {
+  makeSchema,
+  queryType,
+  objectType,
+  idArg,
+  mutationType,
+} from "@nexus/schema";
+import { nexusSchemaPrisma } from "nexus-plugin-prisma/schema";
+import path from "path";
 
+const Company = objectType({
+  name: "Company",
+  definition(t) {
+    // t.id("id", (company, _args, _ctx) => company.id);
+    t.model.id();
+    t.model.symbol();
+    t.model.name();
+    t.model.description();
+  },
+});
 const Query = queryType({
   definition(t) {
-    t.string("name", () => "sejin lee");
+    t.crud.company({
+      resolve: (_root, args, ctx) => {
+        return ctx.prisma.company.findOne({ where: { id: args.where.id } });
+      },
+    });
+    t.crud.companies({ pagination: true, filtering: true });
+    //  t.string("name", () => "sejin lee");
+    // t.field("company", {
+    //   type: Company,
+    //   nullable: true,
+    //   args: { id: idArg() },
+    //   resolve: (_root, { id }, ctx) => {
+    //     return ctx.prisma.company.findOne({ where: { id: Number(id) } });
+    //   },
+    // });
+  },
+});
+
+const Mutation = mutationType({
+  definition(t) {
+    t.crud.createOneCompany();
   },
 });
 export const schema = makeSchema({
-  types: { Query },
+  types: { Query, Company, Mutation },
+  //plugins: [nexusSchemaPrisma()],
+  plugins: [nexusSchemaPrisma({ experimentalCRUD: true })],
+  outputs: {
+    schema: path.join(process.cwd(), "schema.grpahql"),
+    typegen: path.join(process.cwd(), "nexus.ts"),
+  },
+  typegenAutoConfig: {
+    contextType: "Context.Context", //Alias.Type
+    sources: [
+      { source: "@prisma/client", alias: "prisma" },
+      { source: require.resolve("./context"), alias: "Context" },
+    ],
+  },
 });
